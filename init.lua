dca3a0d91a2fd098db2138bf078168c58de49725
misonomi <misonomi0612+github@gmail.com>
2020-03-06 19:22:09 +0900

    storing max y

--- init.lua
+++ init.lua
@@ -1,17 +1,24 @@
+
+-- TODO: handle multiple players
+local max_y = 0
+
 function OnWorldPreUpdate()
 	local players = EntityGetWithTag("player_unit")
 	if players == nil then return end
 
 	for i, p in ipairs(players) do
-		local y = select(2, EntityGetTransform(p))
+		local y = select(2, EntityGetTransform(p)) / 10
+		if y > max_y then max_y = y end
 
-		GamePrint(tostring(GameGetFrameNum()) .. ": " .. tostring(y / 10) .. "m")
+		GamePrint(tostring(GameGetFrameNum()) .. ": " .. tostring(y) .. "m")
+		GamePrint(tostring(GameGetFrameNum()) .. ": " .. tostring(max_y) .. "m")
 	end
 end
 
 
 function OnPlayerSpawned( player_entity )
 	GamePrint("Hello abyss!")
+	max_y = select(2, EntityGetTransform(player_entity))
 	
 	local out = ""
 	for i, c in ipairs(EntityGetAllComponents(player_entity)) do

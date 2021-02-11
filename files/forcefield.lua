fd014f2ab5557721bfb38132bd028b709f5757b8
misonomi <misonomi0612+github@gmail.com>
2021-02-11 14:20:51 +0900

    show forcefield

--- files/forcefield.lua
+++ files/forcefield.lua
@@ -2,26 +2,40 @@ dofile("mods/strains_of_ascension/files/curse.lua")
 
 -- maybe it should be defined in xml? I don't know
 FORCEFIELD_INTERVAL = 200
-FORCEFIELD_FLEX = 50
+FORCEFIELD_FLEX = 25
 
 -- TODO: handle multiple players
 prev_y = 0
 last_ff_y = 0
 
+function reset_ff( y )
+    last_ff_y = y - (y % FORCEFIELD_INTERVAL)
+end
+
 function forcefield_check( player_entity )
     local x, y = EntityGetTransform(player_entity)
-    if y > last_ff_y + FORCEFIELD_INTERVAL then last_ff_y = y - (y % FORCEFIELD_INTERVAL) end
 
+    if y > last_ff_y + FORCEFIELD_INTERVAL + FORCEFIELD_FLEX then reset_ff(y) end
     -- allow teleporting over forcefield
-    if y < last_ff_y and y > prev_y + 1 then last_ff_y = y - (y % FORCEFIELD_INTERVAL) end
+    if y < last_ff_y and prev_y > y + 10 then reset_ff(y) end
 
     if y < last_ff_y - FORCEFIELD_FLEX then
         local curse = curse_table[biome_layer_map[BiomeMapGetName(x, y)]]
         if curse and curse.valid then
             GamePrintImportant("Got the curse of the abyss", curse.desc)
+            GamePlaySound("data/audio/Desktop/event_cues.snd", "event_cues/orb_distant_monster/create", x, y)
             curse.ignite(player_entity)
             curse.valid = false
         end
     end
     prev_y = y
+end
+
+function forcefield_draw( player_entity )
+	local x = select(1, EntityGetTransform(player_entity))
+    local ff_entity = EntityGetWithName("forcefield")
+    if ff_entity == 0 then return end
+    EntitySetTransform(ff_entity, x, last_ff_y, 0, 0)
+
+    local fx, fy = EntityGetTransform(ff_entity)
 end
--- init.lua
+++ init.lua
@@ -3,13 +3,17 @@ dofile("mods/strains_of_ascension/files/forcefield.lua")
 function OnWorldPreUpdate()
 	local players = EntityGetWithTag("player_unit")
 	if players == nil then return end
-	for _, p in ipairs(players) do forcefield_check(p) end
+	for _, p in ipairs(players) do
+		forcefield_check(p)
+		forcefield_draw(p)
+	end
 end
 
 
 function OnPlayerSpawned( player_entity )
 	GamePrintImportant("Hello Abyss!", "you should be careful on ascending")
+	EntityLoad("mods/strains_of_ascension/files/forcefield.xml", 0, 0)
 	local init_y = select(2, EntityGetTransform(player_entity))
-	max_y = init_y
 	prev_y = init_y
+	reset_ff(init_y)
 end

ff152fe196a5740ae51ace4e7546cee3ffeef876
misonomi <misonomi0612+github@gmail.com>
2021-02-11 12:22:20 +0900

    more realistic forcefield

--- files/forcefield.lua
+++ files/forcefield.lua
@@ -1,24 +1,27 @@
 dofile("mods/strains_of_ascension/files/curse.lua")
 
 -- maybe it should be defined in xml? I don't know
-FORCEFIELD_INTERVAL = 20
+FORCEFIELD_INTERVAL = 200
+FORCEFIELD_FLEX = 50
 
 -- TODO: handle multiple players
-max_y = 0
 prev_y = 0
+last_ff_y = 0
 
 function forcefield_check( player_entity )
     local x, y = EntityGetTransform(player_entity)
-    if y > max_y then max_y = y end
+    if y > last_ff_y + FORCEFIELD_INTERVAL then last_ff_y = y - (y % FORCEFIELD_INTERVAL) end
 
-    if y < max_y - FORCEFIELD_INTERVAL and y > prev_y - 1 then
-        local curse = curse_table[biome_layer_map[BiomeMapGetName(x, y)] or 1]
+    -- allow teleporting over forcefield
+    if y < last_ff_y and y > prev_y + 1 then last_ff_y = y - (y % FORCEFIELD_INTERVAL) end
+
+    if y < last_ff_y - FORCEFIELD_FLEX then
+        local curse = curse_table[biome_layer_map[BiomeMapGetName(x, y)]]
         if curse and curse.valid then
             GamePrintImportant("Got the curse of the abyss", curse.desc)
             curse.ignite(player_entity)
             curse.valid = false
         end
     end
-
     prev_y = y
 end
